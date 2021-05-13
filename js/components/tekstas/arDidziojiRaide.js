54ce2ac8aa27f9a4142887106bc6942fc9277d09
Donatas <donkuprys@gmail.com>
2021-05-13 21:10:20 +0300

    arDidzioji

--- js/components/tekstas/arDidziojiRaide.js
+++ js/components/tekstas/arDidziojiRaide.js
@@ -1,14 +1,13 @@
 /**
-*
-* @param {string} text Tekstas
-* @param {number} position Simbolio pozicija, pradedam nuo 1(zmoniu kalba)
-* @returns
-*/
+ * 
+ * @param {string} text Tekstas
+ * @param {number} position Simbolio pozicija, pradedam nuo 1 (zmoniu kalba)
+ * @returns 
+ */
+ function arDidziojiRaide(text, position) {
+    const letter = text[position - 1];                  // g
 
-function arDidziojiRaide (text, position) {
-    const letter = text[position - 1];
-
-    return letter === letter.toUpperCase();
+    return letter === letter.toUpperCase();             // g === G -> false
 }
 
 export { arDidziojiRaide }
